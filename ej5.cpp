522a9c3088764dd8c93d5b0ba165ce21cb82b1f5
Juan Felipe Guevara Olaya <142933841+JuanOlay@users.noreply.github.com>
2025-03-19 13:35:25 -0500

    Create ej5.cpp

---
+++ ej5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct nodo{
+  int x, y;
+  nodo *s;
+};
+
+int main(int argc, char** argv){
+  int N, j, k;
+  nodo ***a, *Aux;
+  cin>> N;
+  a = new nodo **[N];
+  for(i=0;j<N;j++){
+    a[i][j]=new nodo *[N];
+      
+  }
+}
